9341084927e7ee175633b973d805d7d2c87470b5
Adam Johnson <adamj00001@gmail.com>
2023-07-07 18:26:33 -0400

    mod-2-capstone-java-team-2

--- tenmo-server/src/main/java/com/techelevator/tenmo/controller/AccountController.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/controller/AccountController.java
@@ -4,10 +4,15 @@ import com.techelevator.tenmo.model.Account;
 import com.techelevator.tenmo.dao.AccountDao;
 
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.security.Principal;
+import java.util.List;
 
 @RestController
 @PreAuthorize("isAuthenticated()")
+@RequestMapping(path = "/account")
 public class AccountController {
 
     private AccountDao accountDao;
@@ -16,6 +21,32 @@ public class AccountController {
         this.accountDao = accountDao;
     }
 
+    @GetMapping(path = "/balance")
+    public BigDecimal getBalance(Principal principal){
+        BigDecimal balance = accountDao.getBalance(principal.getName());
+        return balance;
+    }
 
+   // @PutMapping(path = "/balance")
+
+//    public BigDecimal updateAccountBalance(Principal principal, @RequestParam BigDecimal balance) {
+//        String username = principal.getName();
+//        BigDecimal balance
+//        Account account = accountDao.getAccountByUserID(userId);
+//        int accountId = account.getAccountId();
+//        accountDao.updateAccountBalance(accountId, balance);
+//        return accountDao.getBalance(accountId);
+//    }
+
+    @GetMapping
+    public Account getAccountByUserId(int userId){
+        Account account = accountDao.getAccountByUserId(userId);
+        return account;
+    }
+
+    @GetMapping
+    public List<Account> getAllAccounts(){
+        return accountDao.getAllAccounts();
+    }
 
 }
